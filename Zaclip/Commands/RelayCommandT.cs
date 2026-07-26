2bef0e6e6c78ca36bb85a546681b464c6d599b85
tomo-k-dragon <tomokdragon@gmail.com>
2026-07-26 09:30:07 +0900

    引数を受け取るコマンドとそうでないコマンドを別にする修正

--- Zaclip/Commands/RelayCommandT.cs
+++ Zaclip/Commands/RelayCommandT.cs
@@ -5,14 +5,12 @@ using System.Windows.Input;
 
 namespace Zaclip.Command
 {
-    public class RelayCommand : ICommand
+    public class RelayCommand<T> : ICommand
     {
-        private readonly Action _execute;
-        private readonly Func<bool>? _canExecute;
+        private readonly Action<T?> _execute;
+        private readonly Func<T?, bool>? _canExecute;
 
-        public RelayCommand(
-            Action execute,
-            Func<bool>? canExecute = null)
+        public RelayCommand(Action<T?> execute, Func<T?, bool>? canExecute = null)
         {
             _execute = execute;
             _canExecute = canExecute;
@@ -20,12 +18,12 @@ namespace Zaclip.Command
 
         public bool CanExecute(object? parameter)
         {
-            return _canExecute == null || _canExecute();
+            return _canExecute == null || _canExecute((T?)parameter);
         }
 
         public void Execute(object? parameter)
         {
-            _execute();
+            _execute((T?)parameter);
         }
 
         public event EventHandler? CanExecuteChanged;
